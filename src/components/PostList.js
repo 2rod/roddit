69c6cde58694345de7a3d9c1a3a7b0bf9f6f9b5a
Rod Reyes <roderick.reyes@gmail.com>
2018-01-15 05:26:47 +0100

    refactor: update references to item

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,36 +5,37 @@ import {
   View,
   Text,
   FlatList,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
 
 import Post from './Post';
 
 class ListItem extends React.PureComponent {
   _onPress = () => {
-    this.props.onPressItem(this.props.item.url);
+    this.props.onPressItem(this.props.item.data.url);
   };
 
   render() {
     return (
       <TouchableOpacity onPress={this._onPress}>
         <View>
-          <Post post={item} />
+          <Post post={this.props.item} />
         </View>
       </TouchableOpacity>
     );
   }
 }
 
-export default class PostList extends React.PureComponent {
+export default class PostList extends Component<{}> {
 
-  _keyExtractor = (item, index) => item.id;
+  _keyExtractor = (item, index) => item.data.id;
 
   _onPressItem = (url: string) => {
     //TODO: open post url in WebView
+    console.log('url', url);
   };
 
-  _renderItem = ({item}) => (
+  _renderItem = ({item, index}) => (
     <ListItem
       item={item}
       onPressItem={this._onPressItem}
