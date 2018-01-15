2b317c26dae565f8ff15a2f5aa645e03c2fb53de
Rod Reyes <roderick.reyes@gmail.com>
2018-01-15 02:09:16 +0100

    feat: map state and actions to props

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,26 +2,56 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  ActivityIndicator
 } from 'react-native';
 
+import { getPosts } from '../actions';
+
+import PostList from '../PostList';
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+    // TODO: fetch initial set of posts
+  }
+
   render() {
+    const loader = this.props.isLoading ?
+      <ActivityIndicator size='large' /> : null;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Roddit
-        </Text>
-        <Text style={styles.instructions}>
-          A minimalist Reddit reader app
-        </Text>
-        <Text style={styles.instructions}>
-          Built by Rod
-        </Text>
+      <View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Roddit
+          </Text>
+          <Text style={styles.instructions}>
+            A minimalist Reddit reader app
+          </Text>
+          <Text style={styles.instructions}>
+            Built by Rod
+          </Text>
+        </View>
+        <View>
+          <Text>/r/Programming</Text>
+          <PostList posts={this.props.posts}/>
+        </View>
+        {loader}
       </View>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  posts: state.posts,
+  category: state.category,
+  isLoading: state.isLoading
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getPosts: (category) => dispatch(getPosts(category))
+});
 
 const styles = StyleSheet.create({
   container: {
