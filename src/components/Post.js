9193e86a7368250a5a5ad91b66dbae9c3c4fa84f
Rod Reyes <roderick.reyes@gmail.com>
2018-01-15 05:24:42 +0100

    refactor: update references and layout

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,20 +10,37 @@ import {
 
 export default class Post extends Component<{}> {
   render() {
-    const post = this.props.post;
+    const post = this.props.post.data;
+    const thumb = post.thumbnail ?
+      <Image style={styles.thumbnail} source={{ uri: post.thumbnail }}/>
+      : null;
     return (
-      <TouchableHighlight>
+      <View style={styles.rowContainer}>
+        {thumb}
+        <Text style={styles.postText}>{post.created}</Text>
+        <Text style={styles.postText}>{post.title}</Text>
         <View>
-          <Image/>
-          <Text>{post.created}</Text>
-          <Text>{post.title}</Text>
-          <View>
-            <Text>{post.author}</Text>
-            <Text>{post.score}</Text>
-            <Text>{post.num_comments}</Text>
-          </View>
+          <Text style={styles.postText}>{post.author}</Text>
+          <Text style={styles.postText}>{post.score}</Text>
+          <Text style={styles.postText}>{post.num_comments}</Text>
         </View>
-      </TouchableHighlight>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  postText: {
+    color: '#333333',
+    fontSize: 12,
+    flex: 1
+  },
+  thumbnail: {
+    width: 20,
+    height: 20
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  },
+});
